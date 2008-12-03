24c360dc43ba82df21476b7160f699a78fd86b53
Richard Carlsson <richardc@klarna.com>
2008-12-03 18:03:09 +0000

    r16489: some editing

--- include/gettext.hrl
+++ include/gettext.hrl
@@ -1,21 +1,25 @@
+%% Client header file for Erlang gettext
 -ifndef(_GETTEXT_HRL).
 -define(_GETTEXT_HRL, true).
 
 -compile({parse_transform,gettext_compile}).
+
+%% Note: Every macro expansion must contain an explicit call to
+%% gettext:key2str(String), so that the parse transform can detect it.
 
 -define(TXT(S), gettext:key2str(S)).
 
 -define(TXT2(S, Lang), gettext:key2str(S, Lang)).
 
 %%%
-%%% This function is similar to ?FTXT but alows S (format string) to use
+%%% This function is similar to ?FTXT but allows S (format string) to use
 %%% args (A) in any order, any number of times or not at all. 
 %%% This is needed for translators of po files to be able to write
-%%% translations with a natural scentence structure. 
+%%% translations with a natural sentence structure. 
 %%% 
 -define(STXT(S, A), gettext_format:stxt(?TXT(S),A)).
 
--define(STXT2(S, A, Lang), gettetxt_format:stxt(?TXT(S, Lang),A)).
+-define(STXT2(S, A, Lang), gettext_format:stxt(?TXT2(S, Lang),A)).
 
 
 %%%
@@ -23,22 +27,20 @@
 %%%
 -define(FTXT(S, A), lists:flatten(io_lib:format(?TXT(S),A))).
 
--define(FTXT2(S, A, Lang), lists:flatten(io_lib:format(?TXT(S, Lang),A))).
+-define(FTXT2(S, A, Lang), lists:flatten(io_lib:format(?TXT2(S, Lang),A))).
 
 
 %%%
-%%% In case the string is used in a javascript context,
-%%% we need to take care of quotes.
-%%%
-%%% I guess we must assume that quotes and backslashes
-%%% always are represented as in good old ASCII...
+%%% In case the string is used in a javascript context, we need to take
+%%% care of quotes. It is assumed that we are working on an encoding
+%%% compatible with ASCII/Latin-1/Unicode.
 %%%
 %%% NB (11 May 2005): DO NOT USE SINGLE QUOTES IN JAVASCRIPT STRINGS !!!
-%%%                   (some browsers doesn't handle escaped single quotes)
+%%%                   (some browsers don't handle escaped single quotes)
 %%%
 %%%
--define(JTXT(S), gettext:quotes(gettext:key2str(S))).
+-define(JTXT(S), gettext:quotes(?TXT(S))).
 
--define(JTXT2(S, Lang), gettext:quotes(gettext:key2str(S, Lang))).
+-define(JTXT2(S, Lang), gettext:quotes(?TXT2(S, Lang))).
 
 -endif.
