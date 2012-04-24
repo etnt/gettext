etnt/gettext

commit 40c923ae804f38ff2e604eca62d347490b5b4d19

Added support for config through app.config

src/gettext_server.erl
@@ -120,10 +120,11 @@ start(CallBackMod, Name) ->
 %%          ignore               |
 %%          {stop, Reason}
 %%--------------------------------------------------------------------
-init([CallBackMod0, Name]) ->
+init([CallBackModConfig, Name]) ->
+	{CallBackMod0, Config} = CallBackModConfig,
     CallBackMod = get_callback_mod(CallBackMod0),
-    GettextDir = get_gettext_dir(CallBackMod),
-    DefLang = get_default_lang(CallBackMod),
+    GettextDir = get_gettext_dir(CallBackMod, Config),
+    DefLang = get_default_lang(CallBackMod, Config),
     TableNameStr = atom_to_list(Name) ++ "_db",
     TableName = list_to_atom(TableNameStr),
     Cache = create_db(TableName, GettextDir),
@@ -271,6 +272,18 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+get_default_lang(CallBackMod, Config) ->
+	case proplists:get_value(default_lang, Config) of
+		undefined -> get_default_lang(CallBackMod);
+		ConfLang -> ConfLang
+	end.
+
+get_gettext_dir(CallBackMod, Config) ->
+	case proplists:get_value(gettext_dir, Config) of
+		undefined -> get_gettext_dir(CallBackMod);
+		ConfDir -> ConfDir
+	end.
 
 db_filename(TableName, GettextDir) ->
     filename:join(GettextDir,  atom_to_list(TableName) ++ ".dets").

src/gettext_sup.erl
@@ -52,7 +52,7 @@ start_link(Options) ->
 %%          {error, Reason}   
 %%----------------------------------------------------------------------
 init([]) ->
-    init([gettext:callback_mod()]);  % just a default that should always work
+    init([{gettext:callback_mod(),application:get_all_env()}]);  % just a default that should always work
 init([CallBackMod]) ->
     GettextServer = {gettext_server,{gettext_server,start_link,[CallBackMod]},
 	      permanent,5000,worker,[gettext_server]},

commit d3948f83c4d27e91a86f255221f6d19141896df1

fix gettext_server:start() that was broken by commit 40c923ae

src/gettext_server.erl
@@ -100,7 +100,7 @@ start_link(CallBackMod, Name) ->
 %%--------------------------------------------------------------------
 
 start() ->
-    start(?MODULE).
+    start({?MODULE, application:get_all_env()}).
 
 start(CallBackMod) ->
     start(CallBackMod, ?SERVER).