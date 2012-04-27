042c5041fa5b10c7458519dab22794c8a879b6bd
Milan Svoboda <msvoboda@ra.rockwell.com>
2012-04-27 07:40:40 +0200

    fix: argument mismatch for macro 'TXT'

--- test/gettext_demo.erl
+++ test/gettext_demo.erl
@@ -8,7 +8,7 @@ hello_world() ->
     hello_world("en").
 
 hello_world(LC) ->
-    io:format("Using ?TXT/2: ~p~n", [?TXT("Hello World", LC)]),
+    io:format("Using ?TXT/2: ~p~n", [?TXT2("Hello World", LC)]),
     put(gettext_language, "sv"),
     io:format("Using ?TXT/1: ~p~n", [?TXT("Hello World")]).
 
